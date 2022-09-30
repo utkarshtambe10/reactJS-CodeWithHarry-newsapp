8a40bcc5c50733058219072b866c9df2c3402f8a
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-09-30 23:37:00 +0530

    adding time, author and news source to newsapp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
         <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:'1', left:'90%'}}>{source}</span>
           <img className="card-img-top" src={!imageUrl ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png" : imageUrl} alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
-              <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
-            </div>
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
+          </div>
         </div>
       </div>
     )
