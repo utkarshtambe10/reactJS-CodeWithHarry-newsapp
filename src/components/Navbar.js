6aaefa834eadc97498ca61538ddc7642f9a2fd4b
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-09-30 22:59:42 +0530

    fetching news category-wise in news-monkey app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
     static propTypes = {}
@@ -8,7 +9,7 @@ export class Navbar extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <a className="navbar-brand" href="/">NewsMonkey</a>
+                    <Link className="navbar-brand" to="/">NewsMonkey</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -16,16 +17,15 @@ export class Navbar extends Component {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
                             </li>
-                            <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/business">Business</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/entertainment">Entertainment</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/general">General</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/health">Health</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/science">Science</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/sports">Sports</a></li>
-                            <li className="nav-item"><a className="nav-link" href="/technology">Technology</a></li>
+                            <li className="nav-item"><Link className="nav-link" to="/business">Business</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/general">General</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                            <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
                         </ul>
                     </div>
                 </nav>
