b340f45456c4754d27c36e8142754b2c640bdf54
Utkarsh Tambe <utkarsh.tambe33@gmail.com>
2022-10-01 00:48:59 +0530

    react component lifecycle and lifecycle methods

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import News from './components/News';
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route
 } from "react-router-dom";
 
@@ -15,7 +15,7 @@ export default class App extends Component {
       <div>
         <Router>
           <Navbar />
-          <Switch>
+          <Routes>
             <Route exact path="/"><News key="general" pageSize={this.pageSize} country="in" category="general" /></Route>
             <Route exact path="/business"><News key="business" pageSize={this.pageSize} country="in" category="business" /></Route>
             <Route exact path="/entertainment"><News key="entertainment" pageSize={this.pageSize} country="in" category="entertainment" /></Route>
@@ -23,7 +23,7 @@ export default class App extends Component {
             <Route exact path="/science"><News key="science" pageSize={this.pageSize} country="in" category="science" /></Route>
             <Route exact path="/sports"><News key="sports" pageSize={this.pageSize} country="in" category="sports" /></Route>
             <Route exact path="/technology"><News key="technology" pageSize={this.pageSize} country="in" category="technology" /></Route>
-          </Switch>
+          </Routes>
         </Router>
       </div>
     )
--- src/components/News.js
+++ src/components/News.js
@@ -16,13 +16,18 @@ export class News extends Component {
     category: PropTypes.string
   }
 
-  constructor() {
-    super();
+  capitalizeFirstLetter = (string)=> {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     }
+    document.title = `${this.props.category} - NewsMonkey`;
   }
 
   async updateNews() {
@@ -54,7 +59,7 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top Headlines</h1>
+        <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading && this.state.articles.map((element) => {
